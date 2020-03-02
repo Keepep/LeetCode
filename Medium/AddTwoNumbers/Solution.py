69cc23823fa74d890798ddf7a3e83c806c585582
kangsinhan <kangsinhan@nate.com>
2020-03-02 15:35:20 +0900

    Update AddTwoNumbers

---
+++ Medium/AddTwoNumbers/Solution.py
@@ -0,0 +1,57 @@
+"""
+You are given two non-empty linked lists representing two non-negative integers.
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+Example:
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+"""
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+   def __init__(self, x):
+     self.val = x
+     self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        sum=list()
+        index=-1
+        up=0
+        while True:
+
+            tmp=ListNode(l1.val+l2.val)
+            if up == 1:
+                tmp.val=tmp.val+1
+                up=0
+            if (tmp.val >=10):
+                tmp.val=tmp.val-10
+                up=1
+
+            sum.append(tmp)
+            if (l1.next==None and l2.next==None):
+                if up==1:
+                    sum.append(ListNode(1))
+                    sum[index].next=sum[index+1]
+                    sum[index+1].next=sum[index+2]
+                    break
+                else:
+                    sum[index].next = sum[index + 1]
+                    break
+            else:
+                if(index>=0):
+                    sum[index].next=sum[index+1]
+
+            index=index+1
+
+            l1 = l1.next
+            l2 = l2.next
+        return sum[0]
---
+++ Medium/AddTwoNumbers/main.py
@@ -0,0 +1,29 @@
+from Medium.AddTwoNumbers.Solution import Solution,ListNode
+
+
+if __name__ == "__main__":
+    l1_1 = ListNode(2)
+    l1_2 = ListNode(4)
+    l1_3 = ListNode(1)
+
+
+    l1_1.next = l1_2
+    l1_2.next = l1_3
+
+
+    l2_1 = ListNode(5)
+    l2_2 = ListNode(6)
+    l2_3 = ListNode(4)
+
+
+    l2_1.next = l2_2
+    l2_2.next = l2_3
+
+    Sol = Solution()
+    sum = Sol.addTwoNumbers(l1_1, l2_1)
+
+    while True:
+        print(sum.val)
+        if (sum.next == None):
+            break
+        sum = sum.next
