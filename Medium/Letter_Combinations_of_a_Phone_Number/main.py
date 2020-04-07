cf0c686be0e77cd6f56b4a456e9bff42179194fd
gangsinhan <kangsinhan@nate.com>
2020-04-07 15:13:55 +0900

    Update "Letter Combination of a Phone Number"

---
+++ Medium/Letter_Combinations_of_a_Phone_Number/main.py
@@ -0,0 +1,8 @@
+from Medium.Letter_Combinations_of_a_Phone_Number.Solution import Solution
+
+
+if __name__ == "__main__":
+    input='23'
+
+    sol=Solution()
+    print('result :{}'.format(sol.letterCombinations(input)))
